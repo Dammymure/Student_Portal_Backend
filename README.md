2066b80b3148aa11f56b1f3e67279a8672bdb736
Dammymure <109584109+Dammymure@users.noreply.github.com>
2023-10-05 09:26:26 +0100

    update on the class point

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Easily get a copy and launch your app on [AirCode](https://aircode.io/) by clicking the button below.
 
-[![Deploy with AirCode](https://aircode.io/aircode-deploy-button.svg)](https://aircode.io/dashboard?owner=Dammymure&repo=Student_Portal_Backend&branch=main&path=&appname=Student%20Portal)
+[![Deploy with AirCode](https://aircode.io/aircode-deploy-button.svg)](https://aircode.io/dashboard?owner=Dammymure&repo=Student_Portal_Backend&path=&appname=Student%20Portal)
--- classes/update.js
+++ classes/update.js
@@ -1,39 +1,85 @@
-// // @see https://docs.aircode.io/guide/functions/
-// const aircode = require('aircode');
-// const {verifyToken} = require("../helper/verifyToken")
+// // // @see https://docs.aircode.io/guide/functions/
+// // const aircode = require('aircode');
+// // const {verifyToken} = require("../helper/verifyToken")
 
+
+// // module.exports = async function (params, context) {
+// //   const tokenUser = await verifyToken(context)
+// //   console.log(tokenUser)
+// //   if(tokenUser != null && tokenUser.isAdmin){
+// //     const {className, students: newStudents, assignment} =params;
+
+// //     const classesTable = aircode.db.table("classes")
+
+// //     try{
+// //       const findClass = await classesTable
+// //       .where({className})
+// //       .findOne() 
+
+// //     if (className = null) findClass.className = tokenUser.className;
+// //     if (assignment) findClass.assignment = assignment;
+
+
+// //       const mergedStudents = [...findClass.students, ...newStudents];
+// //        const result = await classesTable.save({className, students: mergedStudents, assignment })
+
+// //       console.log(findClass)
+// //       console.log("YOUR RSULT IS ",result)
+// //       context.status(200)
+// //       return result 
+// //     }catch(err){
+// //       context.status(500)
+// //       return{"message": err.message}  
+// //     }
+// //   }else{
+// //     context.status(401)
+// //     return{"message":"Token in invalid or user is not authorized"}
+// //   }
+// // };
+
+// const aircode = require('aircode');
+// const { verifyToken } = require("../helper/verifyToken");
 
 // module.exports = async function (params, context) {
-//   const tokenUser = await verifyToken(context)
-//   console.log(tokenUser)
-//   if(tokenUser != null && tokenUser.isAdmin){
-//     const {className, students: newStudents, assignment} =params;
+//   const tokenUser = await verifyToken(context);
 
-//     const classesTable = aircode.db.table("classes")
+//   if (tokenUser != null && tokenUser.isAdmin) {
+//     const { className, students: newStudents, assignment } = params;
 
-//     try{
+//     const classesTable = aircode.db.table("classes");
+
+//     try {
 //       const findClass = await classesTable
-//       .where({className})
-//       .findOne() 
+//         .where({ className })
+//         .findOne();
 
-//     if (className = null) findClass.className = tokenUser.className;
-//     if (assignment) findClass.assignment = assignment;
+//       if (!findClass) {
+//         context.status(404);
+//         return { "message": "Class not found" };
+//       }
 
+//       // If className or assignment are provided, update them
+//       if (className) findClass.className = className;
+//       if (assignment) findClass.assignment = assignment;
 
+//       // Merge the existing students with newStudents
 //       const mergedStudents = [...findClass.students, ...newStudents];
-//        const result = await classesTable.save({className, students: mergedStudents, assignment })
 
-//       console.log(findClass)
-//       console.log("YOUR RSULT IS ",result)
-//       context.status(200)
-//       return result 
-//     }catch(err){
-//       context.status(500)
-//       return{"message": err.message}  
+//       // Update the students field only
+//       findClass.students = mergedStudents;
+
+//       // Save the updated class
+//       const result = await classesTable.save(findClass);
+
+//       context.status(200);
+//       return result;
+//     } catch (err) {
+//       context.status(500);
+//       return { "message": err.message };
 //     }
-//   }else{
-//     context.status(401)
-//     return{"message":"Token in invalid or user is not authorized"}
+//   } else {
+//     context.status(401);
+//     return { "message": "Token is invalid or user is not authorized" };
 //   }
 // };
 
@@ -50,7 +96,7 @@ module.exports = async function (params, context) {
 
     try {
       const findClass = await classesTable
-        .where({ className })
+        .where({ class:tokenUser.className })
         .findOne();
 
       if (!findClass) {
@@ -63,10 +109,9 @@ module.exports = async function (params, context) {
       if (assignment) findClass.assignment = assignment;
 
       // Merge the existing students with newStudents
-      const mergedStudents = [...findClass.students, ...newStudents];
-
-      // Update the students field only
-      findClass.students = mergedStudents;
+      if (newStudents) {
+        findClass.students = [...findClass.students, ...newStudents];
+      }
 
       // Save the updated class
       const result = await classesTable.save(findClass);
